d45c73f6435252eb890ea4299a15c69003c7089d
54255702 <guillermohansen973@gmail.com>
2025-05-05 21:51:37 -0300

    Repartido 4

--- src/ejercicio5.java
+++ src/ejercicio5.java
@@ -16,30 +16,26 @@ public class ejercicio5 {
     public static boolean VerificarCedula(String cedula){
         char numv=cedula.charAt(7);
         int numveri=Character.getNumericValue(numv);
-        System.out.println(numveri);
-        int n0=cedula.charAt(0)*2;
-        int n1=cedula.charAt(1)*9;
-        int n2=cedula.charAt(2)*8;
-        int n3=cedula.charAt(3)*7;
-        int n4=cedula.charAt(4)*6;
-        int n5=cedula.charAt(5)*3;
-        int n6=cedula.charAt(6)*4;
+        int n0= Character.getNumericValue(cedula.charAt(0))*2;
+        int n1= Character.getNumericValue(cedula.charAt(1))*9;
+        int n2= Character.getNumericValue(cedula.charAt(2))*8;
+        int n3= Character.getNumericValue(cedula.charAt(3))*7;
+        int n4= Character.getNumericValue(cedula.charAt(4))*6;
+        int n5= Character.getNumericValue(cedula.charAt(5))*3;
+        int n6= Character.getNumericValue(cedula.charAt(6))*4;
         int n7=n6+n5+n4+n3+n2+n1+n0;
         int n8=(n7%10)-10;
         if(n7%10==0){
             if(numveri==2){
-                System.out.println("si");
                 return true;
             }else{
-                System.out.println("no");
                 return false;
             }
         }
         if(n8 == numveri){
-            System.out.println("SI");
             return true;
+
         }else{
-            System.out.println("NO");
             return false;
         }
     }
